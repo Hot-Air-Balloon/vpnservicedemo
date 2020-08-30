3939f80442e4c23f69a832e47738297768b86b73
mint-mp <liangjing@baicizhan.com>
2020-08-30 18:25:57 +0800

    first

---
+++ app/src/main/java/com/example/vpnservicedemp/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.vpnservicedemp;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.net.VpnService;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    public interface Prefs {
+        String NAME = "VPN_SERVICE_DEMO";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("MyVpnService", "onClick");
+                Intent intent = VpnService.prepare(MainActivity.this);
+                if (intent != null) {
+                    startActivityForResult(intent, 0);
+                } else {
+                    onActivityResult(0, RESULT_OK, null);
+                }
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int request, int result, Intent data) {
+        if (result == RESULT_OK) {
+            Intent test = getServiceIntent();
+            startService(test);
+        }
+        super.onActivityResult(request, result, data);
+    }
+
+    private Intent getServiceIntent() {
+        Log.i("MyVpnService", "getServiceIntent");
+        return new Intent(this, MyVpnService.class);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/vpnservicedemp/MyVpnService.java
@@ -0,0 +1,32 @@
+package com.example.vpnservicedemp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.net.VpnService;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyVpnService extends VpnService {
+    @Override
+    public  void onCreate () {
+        Log.i("MyVpnService", "create");
+        super.onCreate();
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("MyVpnService", "onStartCommand");
+        return START_STICKY;
+//        if (intent != null && ACTION_DISCONNECT.equals(intent.getAction())) {
+//            disconnect();
+//            return START_NOT_STICKY;
+//        } else {
+//            connect();
+//            return START_STICKY;
+//        }
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i("DemoLog", "TestService -> onBind, Thread ID: " + Thread.currentThread().getId());
+        return null;
+    }
+}
