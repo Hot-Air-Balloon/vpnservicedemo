9cd047d7c29441463537f0d6cc9f28f1ca486839
mint-mp <liangjing@baicizhan.com>
2020-09-03 22:56:28 +0800

    test protect fail

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 计划下一步继续完善MyVpnService类。要学习还很多，每天坚持。
 
-## 第二接
+## 第二节
 
 可以建立VPN通道了，读写tun0的数据，想网上说的，读取的数据如果再写回去，又会回到VPNService里来。
 VPN建立以后，核心在于establish返回的ParcelFileDescriptor对象，可以提供输入输出流。
@@ -14,4 +14,25 @@ google的DEMO里实现了UDP的隧道VPN，建立一个UDP链接，并把这个�
 在VPNServer端解析这个UDP数据。
 设置UDP链接用到了DatagramChannel类，接下来估计是了解这个类。并用python重写goolge demo里的Server端。
 这样就能完成一个demo了。
+
+## 第三节
+
+google的demo里，server端新建了一个tun设备，把数据从写入tun设备。利用iptable对写入tun设备对数据做SNET转发。
+```
+// # Enable IP forwarding
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+// # 在nat表的postrouting链建立规则，从10.0.0.0/8原地址来的数据给eth0
+iptables -t nat -A POSTROUTING -s 10.0.0.0/8 -o eth0 -j MASQUERADE
+
+// # 新建一个tun设备
+ip tuntap add dev tun0 mode tun
+
+// # tun设备绑定ip,且和10.0.0.2建立点对点，使中建传输的数据不再被监听获取。这里为什么这样做呢
+// # 是因为10.0.0.2正是客户端的ip,所有写入tun设备的ip报文源IP地址都会是10.0.0.2，？我是这样理解的。
+ifconfig tun0 10.0.0.1 dstaddr 10.0.0.2 up
+```
+
+接下来就是在客服端建立UDP链接，保护起来，和服务端同学。google的demo里是使用CPP写的，这里我们来使用python3实现这个功能。
+功能就是把从客户端UDP链接收到的报文写入tun设备，然后从tun设备读取报文再写回UDP链接。
 
--- app/src/main/java/com/example/vpnservicedemo/MyVpnService.java
+++ app/src/main/java/com/example/vpnservicedemo/MyVpnService.java
@@ -9,10 +9,15 @@ import android.util.Log;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
 
-public class MyVpnService extends VpnService {
+public class MyVpnService extends VpnService implements Runnable {
     private ParcelFileDescriptor m_VPNInterface;
-
+    private final SocketAddress serverAddress = new InetSocketAddress("172.16.167.128", 9090);
+    private static final int MAX_PACKET_SIZE = Short.MAX_VALUE;
     @Override
     public  void onCreate () {
         Log.i("MyVpnService", "create");
@@ -21,11 +26,8 @@ public class MyVpnService extends VpnService {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i("MyVpnService", "onStartCommand");
-        try {
-            this.runVPN();
-        } catch (Exception e) {
-            Log.i("MyVpnService", e.getMessage());
-        }
+        Thread m_VPNThread = new Thread(this, "MyVPNServiceThread");
+        m_VPNThread.start();
         return START_STICKY;
 //        if (intent != null && ACTION_DISCONNECT.equals(intent.getAction())) {
 //            disconnect();
@@ -40,23 +42,49 @@ public class MyVpnService extends VpnService {
         Log.i("DemoLog", "TestService -> onBind, Thread ID: " + Thread.currentThread().getId());
         return null;
     }
-    private void runVPN() throws Exception {
-        this.m_VPNInterface = this.entablishVPN();
-        // 获得网卡的输入输出流
-        FileOutputStream out = new FileOutputStream(this.m_VPNInterface.getFileDescriptor());
-        FileInputStream in = new FileInputStream(this.m_VPNInterface.getFileDescriptor());
-        // 存放每一个IP报文
-        byte[] m_IpPacket = new byte[20000];
-        int size = 0;
-        while (size != -1) {
-            while ((size = in.read(m_IpPacket)) > 0) {
-                Log.i("read length", Integer.toString(size));
-                out.write(m_IpPacket, 0, size);
+
+    @Override
+    public void run() {
+        try {
+            this.m_VPNInterface = this.entablishVPN();
+            // 获得网卡的输入输出流
+            FileOutputStream out = new FileOutputStream(this.m_VPNInterface.getFileDescriptor());
+            FileInputStream in = new FileInputStream(this.m_VPNInterface.getFileDescriptor());
+
+            DatagramChannel tunnel = DatagramChannel.open();
+
+            if (!this.protect(tunnel.socket())) {
+                throw new IllegalStateException("Cannot protect the tunnel");
             }
-            Thread.sleep(100);
+
+            tunnel.connect(serverAddress);
+
+            tunnel.configureBlocking(false);
+
+            // 存放每一个IP报文
+            // byte[] m_IpPacket = new byte[20000];
+            ByteBuffer packet = ByteBuffer.allocate(MAX_PACKET_SIZE);
+            int size = 0;
+            while (size != -1) {
+                while ((size = in.read(packet.array())) > 0) {
+                    Log.i("read length", Integer.toString(size));
+                    tunnel.write(packet);
+                    packet.clear();
+
+                    size = tunnel.read(packet);
+
+                    if (size > 0) {
+                        out.write(packet.array(), 0, size);
+                        packet.clear();
+                    }
+                }
+                Thread.sleep(100);
+            }
+            out.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        out.close();
-        in.close();
     }
     private ParcelFileDescriptor entablishVPN () {
         Builder builder = new Builder();
